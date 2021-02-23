99a4a3770ccac6aac553263fabdd3db3d8307936
AnetaKaczynska <aneta_ka@poczta.onet.pl>
2021-02-23 13:38:10 +0100

    Change focal loss implementation
    Former-commit-id: f34cf0bfa61ddbf0fd574f7f50288a49c10b477e

--- focalloss.py
+++ focalloss.py
@@ -1,38 +1,18 @@
+""" based on https://discuss.pytorch.org/t/focal-loss-for-imbalanced-multi-class-classification-in-pytorch/61289 """
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, alpha=None, size_average=True):
+    def __init__(self, gamma=2, alpha=0.5):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1 - alpha])
-        if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1 - alpha]).cuda()
 
     def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
-        target = target.view(-1, 1)
-
-        logpt = F.log_softmax(input, dim=-1)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
-
-        if self.alpha is not None:
-            if self.alpha.type() != input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0, target.data.view(-1))
-            logpt = logpt * Variable(at)
-
-        loss = -1 * (1 - pt) ** self.gamma * logpt
-        if self.size_average:
-            return loss.mean()
-        else:
-            return loss.sum()
+        ce_loss = F.cross_entropy(input, target, reduction='none') # ce_loss is a vector of -log(pt)
+        pt = torch.exp(-ce_loss) # e^(-log(pt)) = pt
+        focal_loss = (self.alpha * (1-pt)**self.gamma * ce_loss).mean() # mean over the batch
+        return focal_loss
