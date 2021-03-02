483515b2014f5f8367cb6bd564ad3c1f6bb8ebc0
Adam Pardyl <adam@pardyl.com>
2021-03-02 21:12:16 +0100

    New bag creation code for mnist, enable batch-norm
    Former-commit-id: e2b9ab09f8c475f822a8e67ebe86dd77437750a3

--- base_models/small_resnet_features.py
+++ base_models/small_resnet_features.py
@@ -38,10 +38,10 @@ class BasicBlock(nn.Module):
         self.stride = stride
 
     def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = self.conv2(out)
         # out = F.relu(self.bn1(self.conv1(x)))
         # out = self.bn2(self.conv2(out))
-        out = F.relu((self.conv1(x)))
-        out = (self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -60,27 +60,24 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        # self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        # self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        # self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                # nn.BatchNorm2d(self.expansion*planes)
+                nn.BatchNorm2d(self.expansion*planes)
             )
         self.stride = stride
 
     def forward(self, x):
-        # out = F.relu(self.bn1(self.conv1(x)))
-        # out = F.relu(self.bn2(self.conv2(out)))
-        # out = self.bn3(self.conv3(out))
-        out = F.relu((self.conv1(x)))
-        out = F.relu((self.conv2(out)))
-        out = (self.conv3(out))
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -127,7 +124,7 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         # out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu((self.conv1(x)))
+        out = F.relu(self.conv1(x))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
--- datasets/mnist_dataset.py
+++ datasets/mnist_dataset.py
@@ -3,24 +3,29 @@
 import numpy as np
 import torch
 import torch.utils.data as data_utils
+from PIL import Image
 from torchvision import datasets, transforms
 
 
 class MnistBags(data_utils.Dataset):
-    def __init__(self, target_number=9, mean_bag_length=50, var_bag_length=1, num_bag=1000, seed=7, train=True,
+    def __init__(self, target_number=9, mean_bag_length=200, var_bag_length=150, min_bag_size=50, max_bag_size=600,
+                 train_num_bag=1000, test_num_bags=100, seed=7, train=True,
                  push=False):
         self.target_number = target_number
         self.mean_bag_length = mean_bag_length
         self.var_bag_length = var_bag_length
-        self.num_bag = num_bag
+        self.num_bag = train_num_bag if train else test_num_bags
         self.seed = seed
         self.train = train
         self.push = push
+        self.negative_bags = self.num_bag // 2
+        self.min_bag_size = min_bag_size
+        self.max_bag_size = max_bag_size
+
+        self.target_numbers_in_pos_bag_mean = 0.3
+        self.target_numbers_in_pos_bag_std = 0.25
 
         self.r = np.random.RandomState(seed)
-
-        self.num_in_train = 60000
-        self.num_in_test = 10000
 
         self.pil_to_rgb_tensor = transforms.Compose([
             transforms.Lambda(lambda x: x.convert('RGB')),
@@ -32,80 +37,63 @@ class MnistBags(data_utils.Dataset):
         ])
 
         if self.train:
-            self.train_bags_list, self.train_labels_list = self._form_bags()
+            self.bags_list, self.labels_list = self._form_bags()
         else:
-            self.test_bags_list, self.test_labels_list = self._form_bags()
+            self.bags_list, self.labels_list = self._form_bags()
+
+    def _transform_single(self, x):
+        img = Image.fromarray(x.numpy(), mode='L')
+        return self.pil_to_rgb_tensor(img)
 
     def _form_bags(self):
         if self.train:
-            loader = data_utils.DataLoader(
-                datasets.MNIST('data', train=True, download=True, transform=self.pil_to_rgb_tensor),
-                batch_size=self.num_in_train,
-                shuffle=False)
+            data = datasets.MNIST('data', train=True, download=True, transform=self.pil_to_rgb_tensor)
         else:
-            loader = data_utils.DataLoader(
-                datasets.MNIST('data', train=False, download=True, transform=self.pil_to_rgb_tensor),
-                batch_size=self.num_in_test,
-                shuffle=False)
+            data = datasets.MNIST('data', train=False, download=True, transform=self.pil_to_rgb_tensor)
+
+        negative_samples = torch.stack(
+            [self._transform_single(d) for d, t in zip(data.data, data.targets) if t != self.target_number])
+        positive_samples = torch.stack(
+            [self._transform_single(d) for d, t in zip(data.data, data.targets) if t == self.target_number])
 
         bags_list = []
         labels_list = []
-        valid_bags_counter = 0
-        label_of_last_bag = 0
-        numbers, labels = next(iter(loader))
 
-        num_in_case = self.num_in_train if self.train else self.num_in_test
-
-        while valid_bags_counter < self.num_bag:
+        for i in range(self.num_bag):
             bag_length = np.int(self.r.normal(self.mean_bag_length, self.var_bag_length, 1))
-            if bag_length < 1:
-                bag_length = 1
+            bag_length = min(max(bag_length, self.min_bag_size), self.max_bag_size)
 
-            indices = torch.LongTensor(self.r.randint(0, num_in_case, bag_length))
-            labels_in_bag = labels[indices]
-
-            if (self.target_number in labels_in_bag) and (label_of_last_bag == 0):
-                labels_in_bag = labels_in_bag >= self.target_number
-                labels_list.append(labels_in_bag.to(torch.long))
-                bags_list.append(numbers[indices])
-                label_of_last_bag = 1
-                valid_bags_counter += 1
-            elif label_of_last_bag == 1:
-                index_list = []
-                bag_length_counter = 0
-                while bag_length_counter < bag_length:
-                    index = torch.LongTensor(self.r.randint(0, num_in_case, 1))
-                    label_temp = labels[index]
-                    if label_temp.numpy()[0] != self.target_number:
-                        index_list.append(index)
-                        bag_length_counter += 1
-
-                index_list = torch.cat(index_list)
-                labels_in_bag = labels[index_list]
-                labels_in_bag = labels_in_bag >= self.target_number
-                labels_list.append(labels_in_bag.to(torch.long))
-                bags_list.append(numbers[index_list])
-                label_of_last_bag = 0
-                valid_bags_counter += 1
+            if i < self.negative_bags:
+                indices = self.r.randint(0, len(negative_samples), bag_length)
+                bags_list.append(negative_samples[indices])
+                labels_list.append(torch.zeros(bag_length, dtype=torch.long))
             else:
-                pass
+                positive_ratio = float(
+                    self.r.normal(self.target_numbers_in_pos_bag_mean, self.target_numbers_in_pos_bag_std, 1))
+                positive_num = min(max(int(bag_length * positive_ratio), 1), bag_length - 1)
+                negative_num = bag_length - positive_num
+                pos_indices = self.r.randint(0, len(positive_samples), positive_num)
+                neg_indices = self.r.randint(0, len(negative_samples), negative_num)
+                bag = torch.cat([positive_samples[pos_indices], negative_samples[neg_indices]])
+                labels = torch.cat(
+                    [torch.ones(positive_num, dtype=torch.long), torch.zeros(negative_num, dtype=torch.long)])
+                shuffle_indices = self.r.permutation(bag_length)
+                bag = bag[shuffle_indices]
+                labels = labels[shuffle_indices]
+                bags_list.append(bag)
+                labels_list.append(labels)
 
         return bags_list, labels_list
 
     def __len__(self):
         if self.train:
-            return len(self.train_labels_list)
+            return len(self.labels_list)
         else:
-            return len(self.test_labels_list)
+            return len(self.labels_list)
 
     def __getitem__(self, index):
-        if self.train:
-            bag = self.train_bags_list[index]
-            label = torch.LongTensor([max(self.train_labels_list[index])])
-        else:
-            bag = self.test_bags_list[index]
-            label = torch.LongTensor([max(self.test_labels_list[index])])
-
+        bag = self.bags_list[index]
+        label = self.labels_list[index].max().unsqueeze(0)
         if self.push:
             return bag, self.normalize_to_tensor_transform(bag), label
         else:
