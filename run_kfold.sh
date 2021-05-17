8096662bc7d983c0a1882c37a91786dbe4145f52
Jaroslaw Kraus <jkraus@szarancza.capdnet>
2021-05-17 10:47:16 +0200

    updated run_kfold.sh

--- run_kfold.sh
+++ run_kfold.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-for FOLD_ID in 4 8 9; do
+# fold_id=0 busy for test fold
+for FOLD_ID in 1 2 3 4 5 6 7 8 9; do
   for SEED_ID in 0 1 2 3 4; do
     python main.py "$@" --fold_id $FOLD_ID --random_seed_id $SEED_ID
   done
 done
-
