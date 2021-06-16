cc9be4b040e21d54ceb43361721029a38e270139
Adam Pardyl <adam@pardyl.com>
2021-06-16 11:30:18 +0200

    add kfold params

--- run_kfold.sh
+++ run_kfold.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-# fold_id=0 busy for test fold
-for FOLD_ID in 0 1 2 3 4 5 6 7 8 9; do
+START_FOLD_ID=${1:-0}
+END_FOLD_ID=${2:-9}
+
+echo 10-fold crosvalidation, running folds from "$START_FOLD_ID" to "$END_FOLD_ID".
+
+for FOLD_ID in $(seq "$START_FOLD_ID" "$END_FOLD_ID"); do
   for SEED_ID in 0 1 2 3 4; do
-    python main.py "$@" --fold_id $FOLD_ID --random_seed_id $SEED_ID
+    echo CROSSVAL: fold_id: "$FOLD_ID", seed_id: $SEED_ID
+    python main.py "$@" --fold_id "$FOLD_ID" --random_seed_id $SEED_ID
   done
 done
